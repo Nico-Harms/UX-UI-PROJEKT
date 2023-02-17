1c1a8908f65f5925ef42b68f4fe6b29ad22dd872
Nicolai Harms <Nicolai.harms@gmail.com>
2023-02-17 12:46:03 +0100

    countdown - not done

--- script.js
+++ script.js
@@ -10,4 +10,34 @@ document.querySelectorAll(".nav-link").forEach(n => n.
     addEventListener("click", () => {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
-    }))
+    })) 
+
+    // Everything above this is for the hamburger menu 
+
+    const days = document.getElementById('days');
+    const hours = document.getElementById('hours')
+    const minutes = document.getElementById('minutes');
+    const seconds = document.getElementById('seconds');
+
+    const currentYear = new Date().getFullYear();
+
+    const newYearTime = new Date('July 01 $ {currentYear + 1} 00:00:00');
+
+function updateCountdowntime() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60 ) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+console.log(d); 
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+
+}
+
+setInterval(updateCountdowntime, 1000);
